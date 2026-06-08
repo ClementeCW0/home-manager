bd6557e871cc6632f72b3bc61be37e2f62d8b44e
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-06-08 02:03:02 -0400

    New version: 2026-06-08 02:03:02

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.clemente = {
@@ -116,6 +116,7 @@
 
     libinput
     libinput-gestures
+    wmctrl
 
     brightnessctl
     picom
