bdb491ceb6eb6b255be98d06ba5e5ea4303c43c9
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-06-15 17:07:09 -0400

    New version: 2026-06-15 17:07:09

--- configuration.nix
+++ configuration.nix
@@ -179,6 +179,7 @@
         AllowUsers = [ "clemente" ];
         MaxAuthTries = 3;
         PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+	X11Forwarding = true;
       };
     };
 
