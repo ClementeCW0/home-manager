7a468a5f8a0af46e6364fb6a2daf3bf5ab18b2af
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-06-26 12:34:21 -0400

    New version (laptop): 2026-06-26 12:34:21

--- laptop.nix
+++ laptop.nix
@@ -8,8 +8,8 @@
   networking.hostName = "laptop";
   # Use tailscale to forward audio and keyboard/mouse input
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 
-    4713    # PipeWire Audio
+    #4713    # PipeWire Audio
     24800   # Input Leap Keyboard/Mouse
   ];
-  #networking.firewall.allowedTCPPorts = [ 24800 ];
+  networking.firewall.allowedTCPPorts = [ 4713 ];
 }
