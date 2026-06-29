87e3806a1d417a0f7d427f68124aaacb7d4a73e5
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-06-28 21:34:03 -0400

    New version (desktop): 2026-06-28 21:34:03

--- desktop.nix
+++ desktop.nix
@@ -9,9 +9,11 @@
   services.getty.autologinUser = "clemente";
 
   services.teamviewer.enable = true;
+  services.mpd.enable = true; # Remote music hell yeah
 
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 
   	59100 # Audiorelay
+	6600  # MPD
   ];
 }
