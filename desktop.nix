1ba1cf72b95b40933869b33631d7306cf561b27d
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-06-28 21:42:47 -0400

    New version (desktop): 2026-06-28 21:42:47

--- desktop.nix
+++ desktop.nix
@@ -9,7 +9,29 @@
   services.getty.autologinUser = "clemente";
 
   services.teamviewer.enable = true;
-  services.mpd.enable = true; # Remote music hell yeah
+
+########## MPD #################################################################################
+services.mpd = {
+  enable = true;
+  musicDirectory = "/home/clemente/Music"; # Updated to your user path
+  user = "clemente"; 
+  network.listenAddress = "any"; 
+  
+  settings = {
+    # Notice the square brackets added here to make it a list
+    audio_output = [
+      {
+        type = "pipewire";
+        name = "Desktop Audio Server";
+      }
+    ];
+  };
+};
+
+systemd.services.mpd.environment = {
+  XDG_RUNTIME_DIR = "/run/user/1000"; # Double-check your id -u just in case
+};
+################################################################################################
 
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 
