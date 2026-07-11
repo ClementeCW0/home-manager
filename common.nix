08f32fc981053c09479268271b453644e3da66bd
Clemente Wacquez <clemente.wacquez@uc.cl>
2026-07-11 12:29:47 -0400

    New version (laptop): 2026-07-11 12:29:47

--- common.nix
+++ common.nix
@@ -178,6 +178,9 @@ hardware.uinput.enable = true;
   # Greet with Nix logo
   environment.etc."issue".text = builtins.readFile ./nixlogo.txt;
 
+  # Waydroid for android emulation
+  virtualisation.waydroid.enable = true;
+
 
   fonts.packages = with pkgs; [
 	nerd-fonts.jetbrains-mono
